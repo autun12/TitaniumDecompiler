58328cc937205d39226517f196a4b56f46db0bce
autun12 <austindrane56@gmail.com>
2020-12-31 15:20:02 -0600

    Starting TitaniumDecompiler

---
+++ README.md
@@ -0,0 +1,7 @@
+# Titanium Decompiler
+Writing my own decompiler for fun. 
+
+## How to build 
+Clone the repository with `git clone --recursive https://github.com/autun12/TitaniumDecompiler`.
+
+If the repository was cloned non-recursively previously, use `git submodule update --init` to clone the necessary submodules.
---
+++ premake5.lua
@@ -0,0 +1,32 @@
+workspace "TitaniumDecompiler"
+	architecture "x86_64"
+	startproject "TitaniumDecompilernut"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+IncludeDir["GLFW"] = "%{wks.location}/TitaniumDecompiler/vendor/GLFW/include"
+IncludeDir["Glad"] = "%{wks.location}/TitaniumDecompiler/vendor/Glad/include"
+IncludeDir["ImGui"] = "%{wks.location}/TitaniumDecompiler/vendor/imgui"
+
+group "Dependencies"
+	include "vendor/premake"
+	include "TitaniumDecompiler/vendor/GLFW"
+	include "TitaniumDecompiler/vendor/Glad"
+	include "TitaniumDecompiler/vendor/imgui"
+group ""
+
+include "TitaniumDecompiler"
