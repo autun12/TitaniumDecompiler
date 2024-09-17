ea588da77b0387758a10acd32f3abf130e54de6e
autun12 <austindrane56@gmail.com>
2024-09-17 11:18:02 -0500

    new branch

--- TitaniumApplication/premake5.lua
+++ TitaniumApplication/premake5.lua
@@ -1,7 +1,7 @@
 project "TitaniumApplication"
 	kind "ConsoleApp"
 	language "C++"
-	cppdialect "C++20"
+	cppdialect "C++17"
 	staticruntime "on"
 
 	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
